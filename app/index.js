b5805053860b792a3c3716336e1cc15b77b88965
shgtkshruch <shgtk.shruch@gmail.com>
2014-06-19 16:22:03 +0900

    Add test code

--- app/index.js
+++ app/index.js
@@ -8,155 +8,170 @@ var yosay = require('yosay');
 var chalk = require('chalk');
 var wiredep = require('wiredep');
 
-var StaticSiteGenerator = yeoman.generators.Base.extend({
+var StaticSiteGenerator = module.exports = function StaticSiteGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
 
-  askFor: function () {
-    var done = this.async();
+  this.testFramework = options['test-framework'] || 'mocha';
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Blog generator!'));
+  options['test-framework'] = this.testFramework;
 
-    var prompts = [{
-      // Jade template
-      type: 'confirm',
-      name: 'useTemplate',
-      message: 'Do you use Jade template?',
-      default: true
-    }, {
-      // CSS preprosesser
-      type: 'list',
-      name: 'csspreprocessor',
-      message: 'Which do you use CSS preprosesser?',
-      choices: [
-        'Sass',
-        'Stylus'
-        ],
-      default: 'Sass'
-    }, {
-      // JavaSript library
-      type: 'checkbox',
-      name: 'jslib',
-      message: 'What do you use JavaSript library?',
-      choices: [{
-        name: 'Modernizr',
-        value: 'includeModernizr'
-      }, {
-        name: 'HTML5shiv',
-        value: 'includeHTML5shiv'
-      }]
-    }, {
-      // CSS library
-      type: 'checkbox',
-      name: 'csslib',
-      message: 'What do you use CSS library?',
-      choices: [{
-        name: 'Normalize CSS',
-        value: 'includeNormalize'
-      }, {
-        name: 'HTML5 Reset CSS',
-        value: 'includeReset'
-      }]
-    }, {
-      // Cross browser test
-      type: 'confirm',
-      name: 'includeBrowserSync',
-      message: 'Do you use browser-sync?',
-      default: false
-    }];
-
-    this.prompt(prompts, function (answers) {
-      var jslib = answers.jslib;
-      var csslib = answers.csslib;
-
-      var hasjs = function (feat) {
-        return jslib.indexOf(feat) !== -1;
+  this.hookFor('test-framework', {
+    as: 'app',
+    options: {
+      options: {
+        'skip-install': options['skip-install']
       }
-
-      var hascss = function (feat) {
-        return csslib.indexOf(feat) !== -1;
-      }
-
-      this.useTemplate = answers.useTemplate;
-      this.csspreprocessor = answers.csspreprocessor;
-      this.includeBrowserSync = answers.includeBrowserSync;
-      this.includeModernizr = hasjs('includeModernizr');
-      this.includeHTML5shiv = hasjs('includeHTML5shiv');
-      this.includeNormalize = hascss('includeNormalize');
-      this.includeReset = hascss('includeReset');
-
-      done();
-    }.bind(this));
-  },
-
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/styles');
-    this.mkdir('app/scripts');
-    this.mkdir('app/images');
-
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  jade: function () {
-    this.copy('index.jade', 'app/index.jade');
-
-    // layout
-    this.copy('layout.jade', 'app/layout.jade');
-    this.copy('layout/html-head.jade', 'app/layout/html-head.jade');
-    this.copy('layout/ie-script.jade', 'app/layout/ie-script.jade');
-    this.copy('layout/ogp.jade', 'app/layout/ogp.jade');
-
-    // data
-    if (this.useTemplate) {
-      this.mkdir('app/data');
-      this.copy('index.yml', 'app/data/index.yml');
     }
-  },
+  });
 
-  sass: function () {
-    if (this.csspreprocessor === 'Sass') {
-      this.copy('main.scss', 'app/styles/main.scss');
+  this.options = options;
+};
+
+util.inherits(StaticSiteGenerator, yeoman.generators.Base);
+
+StaticSiteGenerator.prototype.askFor = function askFor() {
+  var done = this.async();
+
+  // Have Yeoman greet the user.
+  this.log(yosay('Welcome to the marvelous Blog generator!'));
+
+  var prompts = [{
+    // Jade template
+    type: 'confirm',
+    name: 'useTemplate',
+    message: 'Do you use Jade template?',
+    default: true
+  }, {
+    // CSS preprosesser
+    type: 'list',
+    name: 'csspreprocessor',
+    message: 'Which do you use CSS preprosesser?',
+    choices: [
+      'Sass',
+      'Stylus'
+      ],
+    default: 'Sass'
+  }, {
+    // JavaSript library
+    type: 'checkbox',
+    name: 'jslib',
+    message: 'What do you use JavaSript library?',
+    choices: [{
+      name: 'Modernizr',
+      value: 'includeModernizr'
+    }, {
+      name: 'HTML5shiv',
+      value: 'includeHTML5shiv'
+    }]
+  }, {
+    // CSS library
+    type: 'checkbox',
+    name: 'csslib',
+    message: 'What do you use CSS library?',
+    choices: [{
+      name: 'Normalize CSS',
+      value: 'includeNormalize'
+    }, {
+      name: 'HTML5 Reset CSS',
+      value: 'includeReset'
+    }]
+  }, {
+    // Cross browser test
+    type: 'confirm',
+    name: 'includeBrowserSync',
+    message: 'Do you use browser-sync?',
+    default: false
+  }];
+
+  this.prompt(prompts, function (answers) {
+    var jslib = answers.jslib;
+    var csslib = answers.csslib;
+
+    var hasjs = function (feat) {
+      return jslib.indexOf(feat) !== -1;
     }
-  },
 
-  stylus: function () {
-    if (this.csspreprocessor === 'Stylus') {
-      this.copy('main.styl', 'app/styles/main.styl');
+    var hascss = function (feat) {
+      return csslib.indexOf(feat) !== -1;
     }
-  },
 
-  coffee: function () {
-    this.copy('script.coffee', 'app/scripts/script.coffee');
-  },
+    this.useTemplate = answers.useTemplate;
+    this.csspreprocessor = answers.csspreprocessor;
+    this.includeBrowserSync = answers.includeBrowserSync;
+    this.includeModernizr = hasjs('includeModernizr');
+    this.includeHTML5shiv = hasjs('includeHTML5shiv');
+    this.includeNormalize = hascss('includeNormalize');
+    this.includeReset = hascss('includeReset');
 
-  gulp: function () {
-    this.template('gulpfile.coffee', 'gulpfile.coffee');
-  },
+    done();
+  }.bind(this));
+};
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  },
+StaticSiteGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/styles');
+  this.mkdir('app/scripts');
+  this.mkdir('app/images');
 
-  install: function () {
-    var done = this.async();
-    this.installDependencies({
-      callback: function () {
-        var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
 
-        wiredep({
-          bowerJson: bowerJson,
-          directory: 'bower_components',
-          exclude: ['modernizr.js', 'html5shiv.js'],
-          src: 'app/layout.jade'
-        });
-        done();
-      }.bind(this),
-      skipInstall: this.options['skip-install']
-    });
+StaticSiteGenerator.prototype.jade = function jade() {
+  this.copy('index.jade', 'app/index.jade');
+
+  // layout
+  this.copy('layout.jade', 'app/layout.jade');
+  this.copy('layout/html-head.jade', 'app/layout/html-head.jade');
+  this.copy('layout/ie-script.jade', 'app/layout/ie-script.jade');
+  this.copy('layout/ogp.jade', 'app/layout/ogp.jade');
+
+  // data
+  if (this.useTemplate) {
+    this.mkdir('app/data');
+    this.copy('index.yml', 'app/data/index.yml');
   }
+};
 
-});
+StaticSiteGenerator.prototype.sass = function sass() {
+  if (this.csspreprocessor === 'Sass') {
+    this.copy('main.scss', 'app/styles/main.scss');
+  }
+};
 
-module.exports = StaticSiteGenerator;
+StaticSiteGenerator.prototype.stylus = function stylus() {
+  if (this.csspreprocessor === 'Stylus') {
+    this.copy('main.styl', 'app/styles/main.styl');
+  }
+};
+
+StaticSiteGenerator.prototype.coffee = function coffee() {
+  this.copy('script.coffee', 'app/scripts/script.coffee');
+};
+
+StaticSiteGenerator.prototype.gulp = function gulp() {
+  this.template('gulpfile.coffee', 'gulpfile.coffee');
+};
+
+StaticSiteGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
+
+StaticSiteGenerator.prototype.install = function install() {
+  var done = this.async();
+  this.installDependencies({
+    callback: function () {
+      var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
+
+      wiredep({
+        bowerJson: bowerJson,
+        directory: 'bower_components',
+        exclude: ['modernizr.js', 'html5shiv.js'],
+        src: 'app/layout.jade'
+      });
+      done();
+    }.bind(this),
+    skipInstall: this.options['skip-install']
+  });
+};
---
+++ test/test.js
@@ -0,0 +1,55 @@
+/*global describe, beforeEach, it */
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('static-site generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.app = helpers.createGenerator('static-site:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ]);
+      this.app.options['skip-install'] = true;
+
+      done();
+    }.bind(this));
+  });
+
+  it('the generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    this.app = require('../app');
+  });
+
+  it('creates expected files', function (done) {
+
+    var expected = [
+      // add files you expect to exist here.
+      '.editorconfig',
+      'bower.json',
+      'gulpfile.coffee',
+      'package.json'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      useTemplate: false,
+      csspreprocessor: 'Sass',
+      jslib: ['includeHTML5shiv'],
+      csslib: ['includeNormalize'],
+      includeBrowserSync: false
+    });
+
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+
+});
